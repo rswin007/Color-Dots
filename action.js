b020fd356dd29f68aa5b69973184b99dfdcb9624
Rachel <rswin007@gmail.com>
2017-07-18 16:50:31 -0500

    something

--- action.js
+++ action.js
@@ -1,37 +1,56 @@
-//var body = document.querySelector("body");
-//var button = document.createElement("button");
-//button.innerText = "Click here";
-//button.style.backgroundColor ="blue";
-//body.appendChild(button);
 
-//body.insertAdjacentHTML("beforeend", "<div></div>");
 
 var body = document.querySelector("body");
- 
-for(var i=0; i< 20; i++){
-    var div = document.createElement("div");
 
-     var redValue = getRandomInt(0,255);
-    var greenValue = getRandomInt(0, 255);
-    var blueValue = getRandomInt(0, 255);
 
-    div.style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+function clearOldDots() {
+    var divsToRemove = document.querySelectorAll('div:not(#rachel)');
 
-    var redBorder = getRandomInt(0,255);
-    var greenBorder = getRandomInt(0, 255);
-    var blueBorder = getRandomInt(0, 255);
-
-    div.style.borderColor = `rgb(${redBorder}, ${greenBorder}, ${blueBorder})`;
-    body.appendChild(div);
+    divsToRemove.forEach( function(div) {
+        body.removeChild(div);
+    })
 }
 
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() *  (max - min + 1)) + min;
-        }
+
+function applyDots() {
+    for(var i=0; i< 20; i++){
+        var div = document.createElement("div");
+
+        var redValue = getRandomInt(0,255);
+        var greenValue = getRandomInt(0, 255);
+        var blueValue = getRandomInt(0, 255);
+        var alphaBackground = getRandomArbitrary(0.5, 1);
+
+        div.style.backgroundColor = `rgba(${redValue}, ${greenValue}, ${blueValue}, ${alphaBackground})`;
+
+        var redBorder = getRandomInt(0,255);
+        var greenBorder = getRandomInt(0, 255);
+        var blueBorder = getRandomInt(0, 255);
+        var alphaBorder = getRandomArbitrary(0.5, 1);
+
+        div.style.borderColor = `rgba(${redBorder}, ${greenBorder}, ${blueBorder}, ${alphaBorder})`;
+        body.appendChild(div);
+    }
+}
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() *  (max - min + 1)) + min;
+}
     
-    
-//var i = 1;
-//while(i < 10) {
-    //body.insertAdjacentHTML("beforeend", "<div></div>");
-   // i = i + 1;
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+ 
+var button = document.querySelector("div#rachel");
+
+button.addEventListener("click", function(){
+    clearOldDots();
+    applyDots()
+})
+
+
 
